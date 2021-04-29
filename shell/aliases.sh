3878e8b2a80296fd26c51f21f3ee47b8ec2610f9
Chris Honniball <chris.honniball@avatarfleet.com>
2021-04-29 14:44:08 -0400

    updates

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,8 +4,4 @@ alias la='ls -A'
 alias lla='ls -lA'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES && killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO && killall Finder'
-alias mvim='mvim >& /dev/null'
 alias dc=docker-compose
-
-source '/Users/cjhonniball/www/af30/environment.sh'
-alias build_db='docker-compose up -d python && docker-compose exec python python /af30/scripts/asuite/run_build_db.py'
